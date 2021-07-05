25f5c24ecea6d94263fc3ea5bade111c5d257492
shoom1337 <kirillpavlik@mail.ru>
2021-07-05 21:23:18 +0300

    Revert "parcel as middleware for express"
    This reverts commit d61df1c6db3c34fef369f2e56da7b21b3a169237.

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require("express");
-const Bundler = require("parcel-bundler");
 
-const file = "./static/index.html";
-const PORT = 3000;
+const PORT = 8000;
 
 const app = express();
 
-const bundler = new Bundler(file, { watch: true, cache: false });
-
-app.use(bundler.middleware());
+app.use(express.static("./public"));
 
 app.listen(PORT, function () {
-  console.log(`app successfully started on http://localhost:${PORT}`);
+  console.log(`app listening on http://localhost:${PORT}`);
 });

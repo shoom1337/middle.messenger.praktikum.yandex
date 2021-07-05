d3af63b88757383a3f11d3c1847344e17276caf9
shoom1337 <kirillpavlik@mail.ru>
2021-07-05 18:06:11 +0300

    test3

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, function () {
   console.log(`Server has been started on http://localhost:${PORT}`);

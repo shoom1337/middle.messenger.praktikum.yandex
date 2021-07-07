061beb959cd4a2c7aa38aa9c70d252814966d860
shoom1337 <kirillpavlik@mail.ru>
2021-07-07 22:25:40 +0300

    express now serve dist folder

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
-const Bundler = require("parcel-bundler");
 
 const PORT = 3000;
 
 const app = express();
 
-const bundler = new Bundler("static/index.html", { watch: true, cache: false });
+// TODO to be removed
+// const Bundler = require("parcel-bundler");
+// const bundler = new Bundler("static/index.html", { watch: true, cache: false });
+// app.use(bundler.middleware());
 
-app.use(bundler.middleware());
+app.use(express.static("dist"));
+
+app.get("*", (req, res) =>
+  res.sendFile("index.html", { root: __dirname + "/dist" })
+);
 
 app.listen(PORT, function () {
   console.log(`listening on: http://localhost:${PORT}`);

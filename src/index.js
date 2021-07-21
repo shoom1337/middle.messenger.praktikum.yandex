f40929206191f1c6739d9ea1ed91c4db4528a756
shoom1337 <kirillpavlik@mail.ru>
2021-07-21 11:02:33 +0300

    tsconfig + index.ts

--- src/index.js
+++ src/index.js
@@ -1,52 +1,42 @@
-import serverErrorPage from "./pages/error/500";
-import notFoundPage from "./pages/error/404";
-import loginPage from "./pages/login";
-import registerPage from "./pages/register";
-import profileEditPage from "./pages/profile/edit";
-import passwordEditPage from "./pages/profile/password";
-import avatarEditPage from "./pages/profile/avatar";
-import chatPage from "./pages/chat";
-
-import "./global.scss";
-
-let page;
-
+"use strict";
+exports.__esModule = true;
+var _500_1 = require("./pages/error/500");
+var _404_1 = require("./pages/error/404");
+var login_1 = require("./pages/login");
+var register_1 = require("./pages/register");
+var edit_1 = require("./pages/profile/edit");
+var password_1 = require("./pages/profile/password");
+var avatar_1 = require("./pages/profile/avatar");
+var chat_1 = require("./pages/chat");
+require("./global.scss");
+var page;
 switch (window.location.pathname) {
-  case "/":
-  case "/chat":
-    page = chatPage;
-    break;
-
-  case "/login":
-    page = loginPage;
-    break;
-
-  case "/register":
-    page = registerPage;
-    break;
-
-  case "/edit":
-    page = profileEditPage;
-    break;
-
-  case "/password":
-    page = passwordEditPage;
-    break;
-
-  case "/avatar":
-    page = avatarEditPage;
-    break;
-
-  case "/500":
-    page = serverErrorPage;
-    break;
-
-  case "/404":
-
-  default:
-    page = notFoundPage;
-    break;
+    case "/":
+    case "/chat":
+        page = chat_1["default"];
+        break;
+    case "/login":
+        page = login_1["default"];
+        break;
+    case "/register":
+        page = register_1["default"];
+        break;
+    case "/edit":
+        page = edit_1["default"];
+        break;
+    case "/password":
+        page = password_1["default"];
+        break;
+    case "/avatar":
+        page = avatar_1["default"];
+        break;
+    case "/500":
+        page = _500_1["default"];
+        break;
+    case "/404":
+    default:
+        page = _404_1["default"];
+        break;
 }
-
-const wrapper = document.getElementById("root");
+var wrapper = document.getElementById("root");
 wrapper.innerHTML = page.content;

23c6692e037b5e9189a77fdeac3e9917f6a10f71
shoom1337 <kirillpavlik@mail.ru>
2021-11-07 13:25:43 +0300

    actualized for public

--- src/api/baseAPI.ts
+++ src/api/baseAPI.ts
@@ -14,6 +14,14 @@ const defaultHeaders = {
   "Content-type": "application/json",
 };
 
+function stringifyData(options: ObjectLiteral) {
+  let data = options?.data;
+  if (data && typeof data === "object") {
+    data = JSON.stringify(data);
+  }
+  return data;
+}
+
 class BaseAPI {
   private _fetch: Fetch;
   private _baseURL: string;
@@ -24,38 +32,46 @@ class BaseAPI {
   }
 
   public post(path: string, options: requestData = {}): ObjectLiteral {
+    const data = stringifyData(options);
     return this._fetch
       .post(`${this._baseURL}${path}`, {
-        ...options,
+        data,
         headers: defaultHeaders,
       })
       .then(this._parseResponse);
   }
 
   public get(path: string, options: requestData = {}): ObjectLiteral {
+    const data = stringifyData(options);
     return this._fetch
       .get(`${this._baseURL}${path}`, {
-        ...options,
+        data,
         headers: defaultHeaders,
       })
       .then(this._parseResponse);
   }
 
-  public put(path: string, options: requestData = { headers: null }): ObjectLiteral {
+  public put(
+    path: string,
+    options: requestData = { headers: null },
+    stringifyFlag = true,
+  ): ObjectLiteral {
     const { headers, ...rest } = options;
+    const data = stringifyFlag ? stringifyData(rest) : options.data;
 
     return this._fetch
       .put(`${this._baseURL}${path}`, {
-        ...rest,
+        data,
         headers: headers || defaultHeaders,
       })
       .then(this._parseResponse);
   }
 
   public delete(path: string, options: requestData = {}): ObjectLiteral {
+    const data = stringifyData(options);
     return this._fetch
       .delete(`${this._baseURL}${path}`, {
-        ...options,
+        data,
         headers: defaultHeaders,
       })
       .then(this._parseResponse);
@@ -70,7 +86,7 @@ class BaseAPI {
     try {
       const response = JSON.parse(res.response);
       return response;
-    } catch (e) {
+    } catch (e: any) {
       showAlert({ message: `JSON parse error: ${e.message}`, variant: "error" });
     }
   }
